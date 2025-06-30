f5604f0308d193b51f34e1b4743e1d80812a1ef6
AlenaSazykina <443as.learn.ac@gmail.com>
2025-06-30 20:06:19 +0300

    Change main.java and readme.md Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,54 @@
-/* Программа выводит Вводит текст из консоли и выводит длину текста*/
+/* Калькулятор */
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = getInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = getInt();
+        System.out.println("Сложение " + firstNumber + " + " + secondNumber + " = " + add(firstNumber, secondNumber));
+        System.out.println("Вычетание " + firstNumber + " - " + secondNumber + " = " + sub(firstNumber, secondNumber));
+        System.out.println("Умножение " + firstNumber + " * " + secondNumber + " = " + multi(firstNumber, secondNumber));
+        System.out.println("Деление " + firstNumber + " / " + secondNumber + " = " + division(firstNumber, secondNumber));
     }
+
+    public static int getInt()  {
+        int num;
+        if (scanner.hasNextInt()){
+            num = scanner.nextInt();
+        }
+        else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+
+    public static int add (int firstNumber, int secondNumber) {
+        int operation_add = firstNumber + secondNumber;
+        return operation_add;
+    }
+
+    public static int sub (int firstNumber, int secondNumber) {
+        int operation_sub = firstNumber - secondNumber;
+        return operation_sub;
+    }
+
+    public static int multi (int firstNumber, int secondNumber) {
+        int operation_multi = firstNumber * secondNumber;
+        return operation_multi;
+
+    }
+
+    public static double division (double firstNumber, int secondNumber) {
+        double operation_division = firstNumber / secondNumber;
+        return operation_division;
+
+    }
+
 }
